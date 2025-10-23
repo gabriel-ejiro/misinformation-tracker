ecf18852102c490817d5321e03f8f183fa28ea84
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-10-23 22:31:18 +0200

    Refactor Terraform backend configuration
    Simplified the backend configuration for Terraform.

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "misinfo-tfstate-${var.region}-${var.project_name}"
-    key            = "infra/terraform.tfstate"
-    region         = var.region
-    dynamodb_table = "misinfo-tf-locks"
-    encrypt        = true
-  }
-}
+terraform { backend "s3" {} }
