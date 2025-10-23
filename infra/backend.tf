c2d13bc2adeba07ef9884775c9d50b1a055afe92
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-10-23 22:48:53 +0200

    Simplify Terraform backend configuration
    Removed specific S3 backend configuration for Terraform.

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "misinfo-tfstate-eu-north-1-misinfo"
-    key            = "infra/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "misinfo-tf-locks"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
